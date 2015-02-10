ed990e17aa218876b18b075d053398fb2154caa9
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2015-02-10 16:05:17 -0500

    Recorremos la base de datos de los mensajes.

--- app/src/main/java/com/nullpoint/mensajero/MainActivity.java
+++ app/src/main/java/com/nullpoint/mensajero/MainActivity.java
@@ -2,22 +2,42 @@ package com.nullpoint.mensajero;
 
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.provider.Telephony;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textView);
+        Cursor cursor = getContentResolver().query(Uri.withAppendedPath(Uri.parse("content://sms"), "inbox"), null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+
+            while (cursor.moveToNext()) {
+                Log.i("address", cursor.getString(cursor.getColumnIndex("address")).toString());
+            }
+
+            Toast.makeText(this, "moveToFirst", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "NO moveToFirst", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -40,10 +60,12 @@ public class MainActivity extends ActionBarActivity {
     public void onResume() {
         super.onResume();
 
+        /*
         if (Telephony.Sms.getDefaultSmsPackage(this).equals(getPackageName()))
             Toast.makeText(this, "es el paquete por defecto", Toast.LENGTH_SHORT).show();
         else
             Toast.makeText(this, "NO es el paquete por defecto " + Telephony.Sms.getDefaultSmsPackage(this), Toast.LENGTH_SHORT).show();
+        */
     }
 
     public void setDefault(View view) {
